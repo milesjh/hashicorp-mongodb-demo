02b107cf8f0d4b43b2b9f006de74be52c4f0c610
Miles <miles.harrison@hashicorp.com>
2024-03-19 18:15:20 -0500

    updating mongo creds from hvs

--- 7_mongodbatlas/main.tf
+++ 7_mongodbatlas/main.tf
@@ -4,16 +4,34 @@ terraform {
       source  = "mongodb/mongodbatlas"
       version = "~> 1.15.1"
     }
+
+    hcp = {
+      source = "hashicorp/hcp"
+      version = "~> 0.83.0"
+    }
   }
 }
 
+provider "hcp" {}
+
+# Specify the app in the project (one app_name per data block)
+data "hcp_vault_secrets_app" "mongodb-atlas" {
+  app_name = "mongodb-atlas"
+# Limit the scope to only one or more secrets in the app
+# secret_name = “secret-name-1” “secret-name-2”
+}
+
+# Replace your existing secret references with
+# data.hcp-vault-secrets_app.mongodb-atlas.secret-name
+
+
 provider "mongodbatlas" {
-  public_key  = var.MONGODB_ATLAS_PUBLIC_KEY
-  private_key = var.MONGODB_ATLAS_PRIVATE_KEY
+  public_key  = data.hcp-vault-secrets_app.mongodb-atlas.MONGODB_ATLAS_PUBLIC
+  private_key = data.hcp-vault-secrets_app.mongodb-atlas.MONGODB_ATLAS_PRIVATE
 }
 
 resource "mongodbatlas_cluster" "lifetimecluster" {
-  project_id                  = var.MONGODB_PROJECT_ID
+  project_id                  = data.hcp-vault-secrets_app.mongodb-atlas.MONGODB_PROJECT
   name                        = "TerraformAtLifetime"
   cluster_type                = "REPLICASET"
   provider_name               = "AWS"
